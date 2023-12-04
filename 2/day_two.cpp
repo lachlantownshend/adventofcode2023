e3ca1fefd99d7ae69d017826e247cce5decad314
Lachlan Townshend <lachlan.townshend@gmail.com>
2023-12-04 22:39:36 +1100

    Day 2 complete

--- 2/day_two.cpp
+++ 2/day_two.cpp
@@ -1,58 +1,62 @@
 #include <fstream>
 #include <iostream>
-#include <map>
+#include <unordered_map>
 
 int main(void){
 
     std::ifstream infile("input.txt");
     std::string line;
 
-    int id, blue, red, green;
-
-    int MAX_RED = 12;
-    int MAX_GREEN = 13;
-    int MAX_BLUE = 14;
-    
-    std::map<char, uint16_t> max_val = {
-        {'r', 12},
-        {'g', 13},
-        {'b', 14}
+    // Store the minimum values for each colour in a map
+    std::unordered_map<char, uint16_t> min_val = {
+        {'r', 0},
+        {'g', 0},
+        {'b', 0}
     };
 
-    uint16_t num, left, right;
-    uint16_t total = 0;
+    uint16_t num, left, right, id;
+    uint64_t total = 0;
     char colour;
-    bool valid;
 
+    // For each line in the file
     while(std::getline(infile, line)){
 
+        // Start at the first number
         left = line.find(":") + 2;
 
-        valid = true;
+        min_val = {
+            {'r', 0},
+            {'g', 0},
+            {'b', 0}
+        };
 
-        while(valid && left < line.length()){
+        // Go until the end of the line
+        while(left < line.length()){
 
+            // Look for the final digit of the number
             right = left + 1;
-
             while(isdigit(line[right])){right++;};
             if(right > line.length()){break;}
 
+            // Take the slice containing the number
             num = stoi(line.substr(left, right - left));
 
+            // The colour is two chars after the number. Update the minimum value
             colour = (char)line[right+1];
-
-            if(num > max_val[colour]){
-                valid = false;
+            if(num > min_val[colour]){
+                min_val[colour] = num;
             }
 
+            // We can start looking from the end of the next number
             left = right;
 
+            // Move until we have found the next digit
             while(left != line.length() && !isdigit(line[left])){left++;};
         }
-        if(valid){
-            id = stoi(line.substr(5, line.find(':') - 5));
-            total += id;
-        }
+
+        // Sum the powers
+        total += min_val['r'] * min_val['g'] * min_val['b'];
     }
+
     std::cout << "Output: " << total << '\n';
 }
